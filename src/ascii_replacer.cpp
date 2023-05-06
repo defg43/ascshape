e256f7e8c8d63d32102611109bc5affdf06c24f7
defg43 <nino.obradovic22@gmail.com>
2023-05-06 14:17:38 +0200

    restructuring, currently broken

--- src/ascii_replacer.cpp
+++ src/ascii_replacer.cpp
@@ -3,6 +3,15 @@
 #include <string>
 #include <algorithm>
 #include <regex>
+#include <assert.h>
+
+#define DEBUG 0
+
+#if DEBUG == 1
+#define debug(...) printf(__VA_ARGS__)
+#else
+#define debug(...) 
+#endif
 
 void print_usage() {
     printf("Usage: ascii_art [mask_file] [source_file]\n"); 
@@ -31,6 +40,7 @@ int main(int argc, char* argv[]) {
 
     if (!mask_file || !source_file) {
         std::cerr << "Error opening files\n";
+        std::cerr << (!mask_file) << " and the source is " << (!mask_file) << '\n';
         return 1;
     }
 
@@ -72,9 +82,6 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-
-
-
     int source_index = 0;
     int mask_index = 0;
 
@@ -89,14 +96,116 @@ int main(int argc, char* argv[]) {
     mask_str = buffer.str();
     int source_code_index;
 
-    for(int index = 0; auto &maskchar : mask_str) {
+    printf(source_str.c_str());
+    printf("\n");
+    shaped_output = mask_str;
 
+    // main replacing logic
+    for(int index = 0; index < mask_str.length(); index++) {
+        // if a newline is encountered in the mask copy it over to the output
+        if (mask_str[index] == '\n') {
+                shaped_output[index] = '\n';
+                index++;
+                debug("[DEBUG] whitespace, skipping\n");
+            }
+        
         // look ahead in mask and find number until next ' '
-        int mask_current_remaining_word_length =
-            mask_str.find(' ', index);
-        // compare to source_code_str and look if the next word fits in
-        int source_code_current_remaining_word_length =
-                source_str.find(' ', source_code_index);
+        // this is the space that is still avaible in the mask
+        int mask_word_length =
+            std::min(mask_str.find(' ', index), mask_str.find('\n', index)) - index;
+        // get length of the next token from the source code
+        int source_code_word_length = (source_str[source_code_index] == ' ')?
+            source_str.find(' ', source_code_index + 1) - source_code_index:
+            source_str.find(' ', source_code_index) - source_code_index;
+        
+        // if DEBUG is set to 1 this just logs indexes
+        debug("[DEBUG] index: %d, word_lenght: %d, src_word_len: %d, src_index: %d, src_len: %d\n", 
+                index, mask_word_length, source_code_word_length, 
+                source_code_index, source_str.length());
+
+            // this means we havent reached the end of the source code
+            if(source_code_index < source_str.length()) {
+                // try to fit source code token into mask
+                if (mask_word_length >= source_code_word_length) { 
+                // enough space for token
+                    source_code_index++; // copy from src to ouput
+                    shaped_output[index] = source_str[source_code_index]; 
+                } else { // not enough space for token handle edge case here
+
+                    debug("[DEBUG] mask: %d, src: %d ", mask_word_length, source_code_word_length);
+                    // edgecase function here and check if we are inside of a comment
+                    debug("[DEBUG] we had %d places left\n", mask_word_length);
+                    bool we_have_more_problems = false;
+                    switch (mask_word_length) {
+                    
+                    case 1:
+                        shaped_output[index] = ' ';
+                        index++;
+                        break;
+                    #if 0
+                    case 2:
+                        shaped_output[index] = '/';
+                        shaped_output[index + 1] = '*';
+                        index += 2;
+                        we_have_more_problems = false;
+                        printf("\n\nentering loop\n\n");
+                        do {
+                            while(mask_str[index] == ' ' || mask_str[index] == '\n' 
+                                && index < mask_str.length()) {
+                                index += 2;
+                            }
+                            if(index == mask_str.length()) {
+                                std::cerr << "exceded lenght" << '\n';
+                                exit(EXIT_FAILURE);
+                            }
+                            int section_lenght = mask_str.find(index, ' ') - index;
+                            assert(section_lenght > 0);
+
+                            if (source_str[source_code_index] == ' ') source_code_index++;
+                            int next_token_lenght = source_str.find(source_code_index, ' ') -
+                                source_code_index + 2; // +2 because we want to fir "* /"
+
+                            if (section_lenght >= next_token_lenght) {
+                                shaped_output[index] = '*';
+                                shaped_output[index + 1] = '/';
+                                index += 2; 
+                            } else {
+                                // fill section with comments *
+                                while (mask_str[index] == '#')
+                                {
+                                    if (mask_str[index] != '\n') 
+                                        shaped_output[index] = '*';                                
+                                    index++;                             
+                                }  
+                                we_have_more_problems = true;                   
+                            }
+                        } while(we_have_more_problems);
+                        break;
+                    #endif
+                    default:
+                        break;
+                    }
+                } 
+            }
+
+
+            // if there is enough space in the mask paste in token
+            if (mask_word_length >= source_code_word_length) {
+                if(source_code_index < source_str.length()) {
+                    source_code_index++;
+                    shaped_output[index] = source_str[source_code_index];
+                } else // if we get to the lenght of the source code we are done  
+                    printf("%s\n", shaped_output.c_str());
+            }
+
+            // we unable to paste a token from the src into the mask
+            // handle all cases here and turn this into a function later
+            if (mask_word_length < source_code_word_length)
+            {
+                
+            }
+            
+
             /* this can be expanded to include other opportunities to split 
             *  the source code tokens
             *  space characters are obviously earsed from the source code as
@@ -105,37 +214,14 @@ int main(int argc, char* argv[]) {
             *  if the source code contains "func ()" it can obviously be merged together
             *  but that is harder to implement
             */
-        
         // if not call edge case function with the string and position 
-        
         // and try to fill in a comment this can also fail as the comment may reduce
         // availible characters which may result in the string not fitting in       
     }
-    
+    // when done output the string to the terminal
+    printf("\n[RESULT] result: \n%s", shaped_output.c_str());
 
-    [[deprecated]] {
-        while (std::getline(mask_file, mask_line)) {
-    
-        int line_length = mask_line.length(); // no
-    
-        for (int i = 0; i < line_length; i++) { // do for global
-            if (mask_line[i] == '#') {
-                if (source_index < source_length) {
-                    result_line += source_str[source_index++];
-                }
-                else {
-                    result_line += ' ';
-                }
-            }
-            else {
-                result_line += mask_line[i];
-            }
-        }
-        std::cout << result_line << "\n"; // :|
-        result_line.clear();
-        }
-    }
-
+    // cleanup
     mask_file.close();
     source_file.close();
 
